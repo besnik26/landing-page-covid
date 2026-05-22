43af8246e881c05a9d81149e848e25b36235247b
besnik <besnikbeka2001@gmail.com>
2026-05-22 15:56:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="main">
+      <div className="container">
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
